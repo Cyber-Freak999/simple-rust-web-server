0dbd0a54c99d249b39c3362205e0d040663a056b
Cyber-Freak999 <cyberfreak999@protonmail.com>
2024-01-14 22:25:39 +0100

    Returning Webpages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -13,6 +14,7 @@ fn main() {
     }
 }
 
+/// This function is used to prints out incoming requests
 // fn handle_connection(mut stream: TcpStream) {
 //     let buf_reader = BufReader::new(&mut stream);
 //     let http_request: Vec<_> = buf_reader
@@ -24,15 +26,34 @@ fn main() {
 //     println!("Request: {:#?}", http_request);
 // }
 
+/// This function is used to write HTTP requests
+// fn handle_connection(mut stream: TcpStream) {
+//     let buf_reader = BufReader::new(&mut stream);
+//     let http_request: Vec<_> = buf_reader
+//         .lines()
+//         .map(|result| result.unwrap())
+//         .take_while(|line| !line.is_empty())
+//         .collect();
+
+//     let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+//     stream.write_all(response.as_bytes()).unwrap();
+// }
+
+/// This function is used to write HTTP requests with HTML
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    let _http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
