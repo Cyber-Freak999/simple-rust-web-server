d18185d0bfbee180132d217b215fc38287eb43b4
Cyber-Freak999 <cyberfreak999@protonmail.com>
2024-01-14 22:16:26 +0100

    Writing HTTP Response

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,17 @@ fn main() {
     }
 }
 
+// fn handle_connection(mut stream: TcpStream) {
+//     let buf_reader = BufReader::new(&mut stream);
+//     let http_request: Vec<_> = buf_reader
+//         .lines()
+//         .map(|result| result.unwrap())
+//         .take_while(|line| !line.is_empty())
+//         .collect();
+
+//     println!("Request: {:#?}", http_request);
+// }
+
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
@@ -21,5 +32,7 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", http_request);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
